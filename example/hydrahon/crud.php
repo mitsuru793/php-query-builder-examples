4170f224ecac969c100227e41b9d6d27f285d8b2
mitsuru793 <mitsuru793@gmail.com>
2018-11-03 13:52:51 +0900

    add example/hydrahon/

--- example/hydrahon/crud.php
+++ example/hydrahon/crud.php
@@ -1,16 +1,19 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+const ROOT = __DIR__ . '/../../';
+
+require_once ROOT . '/vendor/autoload.php';
 
 use Example\Hydrahon\Model;
 use Example\Hydrahon\Post;
 use Example\Hydrahon\PostStatus;
+use Example\Hydrahon\User;
 use Example\Util\DbConfig;
 
 function main()
 {
-    $env = new \Dotenv\Dotenv(__DIR__ . '/../');
+    $env = new \Dotenv\Dotenv(ROOT);
     $env->overload();
 
     $config = new DbConfig();
@@ -18,12 +21,12 @@ function main()
     Model::$pdo = $pdo;
 
     // ユーザー登録
-    $author = new \Example\Hydrahon\User();
+    $author = new User();
     $author->name = 'Mike';
     $author->save();
 
     // ユーザーが記事を作成
-    $author = \Example\Hydrahon\User::findByName('Mike')[0];
+    $author = User::findByName('Mike')[0];
     $post = new Post();
     $post->authorId = $author->id;
     $post->title = 'First Post';
